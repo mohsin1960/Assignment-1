7b23c6064f36a0a31a3a2a3712c1309cd98984f6
mohsin1960 <i201960@nu.edu.pk>
2025-03-17 17:52:32 +0500

    files added

---
+++ README.md
@@ -0,0 +1,18 @@
+****Assignment****
+
+
+**Task-1**
+
+2 files in Task-1 (date-time.py, Dockerfile)
+
+**Building the docker image for above**
+
+docker build -t datetime-api .
+
+**Running the Container** 
+
+docker run -p 8000:8000 datetime-api
+
+Now Access on browser
+
+
---
+++ Task-1/date-time.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/datetime', methods=['GET'])
+def get_datetime():
+    return jsonify({"datetime": datetime.utcnow().isoformat()})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
